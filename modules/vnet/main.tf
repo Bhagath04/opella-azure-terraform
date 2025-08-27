d3eb8aa572340851da02f08c77e07a625fbe3171
Bhagath04 <126044720+Bhagath04@users.noreply.github.com>
2025-08-27 10:49:54 +0530

    Updates

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -36,35 +36,3 @@ resource "azurerm_network_security_rule" "this" {
   network_security_group_name = azurerm_network_security_group.this.name
 }
 
-module "vnet" {
-  source              = "./modules/vnet"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-
-  vnet_name     = "${local.name_prefix}-vnet"
-  address_space = ["10.0.0.0/16"]
-
-  subnets = {
-    subnet1 = {
-      address_prefix = "10.0.1.0/24"
-    }
-    subnet2 = {
-      address_prefix = "10.0.2.0/24"
-    }
-  }
-
-  nsg_rules = {
-    allow_ssh = {
-      priority                   = 100
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "22"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-  }
-
-  tags = local.tags
-}
