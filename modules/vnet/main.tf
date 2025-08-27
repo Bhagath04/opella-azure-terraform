92a651b3429c298e03f0cb18b2cf1cd95df75256
Bhagath04 <126044720+Bhagath04@users.noreply.github.com>
2025-08-27 10:38:55 +0530

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,38 +1,60 @@
-resource "azurerm_virtual_network" "this" {
-  name                = var.vnet_name
-  address_space       = var.address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
-resource "azurerm_subnet" "this" {
-  for_each             = var.subnets
-  name                 = each.key
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = [each.value.address_prefix]
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_network_security_group" "this" {
-  name                = "${var.vnet_name}-nsg"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
+locals {
+  name_prefix = "myproject"
+  tags = {
+    environment = "dev"
+    project     = "terraform-vnet"
+  }
 }
 
-resource "azurerm_network_security_rule" "this" {
-  for_each = var.nsg_rules
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.name_prefix}-rg"
+  location = "East US"
+  tags     = local.tags
+}
 
-  name                        = each.key
-  priority                    = each.value.priority
-  direction                   = each.value.direction
-  access                      = each.value.access
-  protocol                    = each.value.protocol
-  source_port_range           = each.value.source_port_range
-  destination_port_range      = each.value.destination_port_range
-  source_address_prefix       = each.value.source_address_prefix
-  destination_address_prefix  = each.value.destination_address_prefix
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.this.name
+module "vnet" {
+  source              = "./modules/vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  vnet_name     = "${local.name_prefix}-vnet"
+  address_space = ["10.0.0.0/16"]
+
+  subnets = {
+    subnet1 = {
+      address_prefix = "10.0.1.0/24"
+    }
+    subnet2 = {
+      address_prefix = "10.0.2.0/24"
+    }
+  }
+
+  nsg_rules = {
+    allow_ssh = {
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  }
+
+  tags = local.tags
 }
