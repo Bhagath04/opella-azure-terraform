b07b8d10ad7f54c69aef38cb8b3c9f1f7e048382
Bhagath04 <126044720+Bhagath04@users.noreply.github.com>
2025-08-27 09:57:37 +0530

    Changed provider

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -1,16 +1,12 @@
 terraform {
-  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.100.0"
-    }
-    random = {
-      source = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
-  backend "local" {}
-  # For team use, switch to AzureRM backend and storage account if desired.
+
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
--- envs/prod/providers.tf
+++ envs/prod/providers.tf
@@ -1,13 +1,14 @@
 terraform {
-  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.100.0"
+      version = "~> 3.0"
     }
-    random = { source = "hashicorp/random" }
   }
-  backend "local" {}
+
+  required_version = ">= 1.1.0"
 }
 
-provider "azurerm" { features {} }
+provider "azurerm" {
+  features {}
+}
