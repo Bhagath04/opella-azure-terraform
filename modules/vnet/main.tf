22e084a721fd02aed60369167a0138f9db3f0d3e
Bhagath04 <126044720+Bhagath04@users.noreply.github.com>
2025-08-27 09:51:45 +0530

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,54 +1,76 @@
-provider "azurerm" {
-  features {}
+variable "resource_group_name" {}
+variable "location" {}
+variable "vnet_name" {}
+variable "address_space" {
+  type = list(string)
+}
+variable "subnet_name" {}
+variable "subnet_prefixes" {
+  type = list(string)
 }
 
 resource "azurerm_virtual_network" "this" {
-  name                = var.name
+  name                = var.vnet_name
+  address_space       = var.address_space
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = var.address_space
-  tags                = var.tags
 }
 
-# Create subnets
 resource "azurerm_subnet" "this" {
-  for_each             = var.subnets
-  name                 = each.key
+  name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = each.value.address_prefixes
+  address_prefixes     = var.subnet_prefixes
 }
 
-# Optional NSG per subnet
-resource "azurerm_network_security_group" "subnet" {
-  for_each            = { for k, v in var.subnets : k => v if try(v.nsg_enabled, false) }
-  name                = "${each.key}-nsg"
+resource "azurerm_network_security_group" "this" {
+  name                = "${var.vnet_name}-nsg"
   location            = var.location
   resource_group_name = var.resource_group_name
-  tags                = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = azurerm_subnet.this.id
+  network_security_group_id = azurerm_network_security_group.this.id
 }
 
 resource "azurerm_network_security_rule" "subnet_rules" {
-  for_each = {
-    for s, cfg in var.subnets : s => cfg if try(cfg.nsg_enabled, false)
+  for_each                    = {
+    "ssh" = {
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+    "http" = {
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
   }
 
-  count                      = length(var.nsg_rules)
-  name                       = var.nsg_rules[count.index].name
-  priority                   = var.nsg_rules[count.index].priority
-  direction                  = var.nsg_rules[count.index].direction
-  access                     = var.nsg_rules[count.index].access
-  protocol                   = var.nsg_rules[count.index].protocol
-  source_port_range          = var.nsg_rules[count.index].source_port_range
-  destination_port_range     = var.nsg_rules[count.index].destination_port_range
-  source_address_prefix      = var.nsg_rules[count.index].source_address_prefix
-  destination_address_prefix = var.nsg_rules[count.index].destination_address_prefix
-  resource_group_name        = var.resource_group_name
-  network_security_group_name= azurerm_network_security_group.subnet[each.key].name
+  name                        = "${each.key}-rule"
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_port_range           = each.value.source_port_range
+  destination_port_range      = each.value.destination_port_range
+  source_address_prefix       = each.value.source_address_prefix
+  destination_address_prefix  = each.value.destination_address_prefix
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.this.name
 }
 
-resource "azurerm_subnet_network_security_group_association" "assoc" {
-  for_each                  = { for k, v in var.subnets : k => v if try(v.nsg_enabled, false) }
-  subnet_id                 = azurerm_subnet.this[each.key].id
-  network_security_group_id = azurerm_network_security_group.subnet[each.key].id
+output "subnet_id" {
+  value = azurerm_subnet.this.id
 }
