c7bac73d27ba1eab78a762282ff74735bc12f658
Bhagath04 <126044720+Bhagath04@users.noreply.github.com>
2025-08-27 10:47:27 +0530

    updated

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -20,10 +20,20 @@ locals {
   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${local.name_prefix}-rg"
-  location = "East US"
-  tags     = local.tags
+resource "azurerm_network_security_rule" "this" {
+  for_each = var.nsg_rules
+
+  name                        = each.key
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_port_range           = each.value.source_port_range
+  destination_port_range      = each.value.destination_port_range
+  source_address_prefix       = each.value.source_address_prefix
+  destination_address_prefix  = each.value.destination_address_prefix
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.this.name
 }
 
 module "vnet" {
